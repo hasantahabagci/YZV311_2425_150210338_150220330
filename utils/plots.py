53b3e21371c1bc09ea1cc031579b0e28d27f3b6b
Hasan Taha Bagci <bagcih21@itu.edu.tr>
2025-01-03 22:55:02 +0300

    Update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -2,20 +2,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import ConfusionMatrixDisplay
 
-def plotdist(df, col, mx):
-    """
-    This function plots the distribution of a column
-    :param df: dataframe
-    :param col: column name
-    :return: None
-    """
-    plt.figure(figsize=(10, 6))
-    sns.distplot(df[col], kde=True, label='skewness : %.2f'%(df[col].skew()))
-    plt.vlines(df[col].mean(), 0, mx, colors='r', label='mean')
-    plt.text(df[col].mean(), mx*1.01, "mean : "+ str(round(df[col].mean(),2)), fontsize=12)
-    plt.title(col.upper() + " DISTRIBUTION")
-    plt.legend()
-    plt.show()
 
 def plotbox(df, col):
     """
